ab8ebf47a6777f7208a0723d6bba0f949cee647a
sarcino <sarka.sevci@gmail.com>
2018-01-10 14:19:14 +0100

    script - link, styles, logo

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
     var url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=30778564.c014a64.4b34293450c7470e9599b9a3a76e7271";
 
     $.getJSON(url, function (json) {
-        for (var i = 0; i < 20; i++) {
-            $("#feed").append("<img src = '" + json.data[i].images.standard_resolution.url + "' " + "alt='" + json.data[i].caption.text + "'>");
+        for (var i = 0; i < 18; i++) {
+            $("#feed").append("<figure><a href='" + json.data[i].link + "' target='_blank'><img src = '" + json.data[i].images.standard_resolution.url + "' " + "alt='" + json.data[i].caption.text + "'><figcaption><p><img src='instagram-logo.png'><br />" + json.data[i].caption.text + "</p></figcaption></a></figure>");
         }
     })
 
