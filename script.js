82460e5fb57aa54976b6cd273e0bff5461434c00
sarcino <sarka.sevci@gmail.com>
2019-02-17 10:59:56 +0100

    CORS solved by ajax and jsonp

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-    
-    console.log("Hi, stranger!");
-    
-    $('body').css('display', 'none');
-    $('body').fadeIn(2100);
 
-    // Instagram API
-    var url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=30778564.c014a64.4b34293450c7470e9599b9a3a76e7271";
+    $.ajax({
+        type: "GET",
+        url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=30778564.c014a64.4b34293450c7470e9599b9a3a76e7271",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function (data) {
+            console.log(data);
+            for (var i = 0; i < 18; i++) {
+                $("#feed").append("<figure><a href='" + data.data[i].link + "' target='_blank'><img src = '" + data.data[i].images.standard_resolution.url + "' " + "alt='" + data.data[i].caption.text + "'><figcaption><p><img src='instagram-logo.png'><br />" + data.data[i].caption.text + "<br /><span><img src='heart.png'>" + data.data[i].likes.count + "</span></p></figcaption></a></figure>");
+            }
+        },      
 
-    $.getJSON(url, function (json) {
-        for (var i = 0; i < 18; i++) {
-            $("#feed").append("<figure><a href='" + json.data[i].link + "' target='_blank'><img src = '" + json.data[i].images.standard_resolution.url + "' " + "alt='" + json.data[i].caption.text + "'><figcaption><p><img src='instagram-logo.png'><br />" + json.data[i].caption.text + "<br /><span><img src='heart.png'>" + json.data[i].likes.count + "</span></p></figcaption></a></figure>");
-        }
-    })
+    });
 
-
+    // END OF READY FUN
 });
---
+++ script1.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    
+    console.log("Hi, stranger!");
+    
+    $('body').css('display', 'none');
+    $('body').fadeIn(2100);
+
+    // Instagram API
+    var url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=30778564.c014a64.4b34293450c7470e9599b9a3a76e7271";
+
+    $.getJSON(url, function (json) {
+        for (var i = 0; i < 18; i++) {
+            $("#feed").append("<figure><a href='" + json.data[i].link + "' target='_blank'><img src = '" + json.data[i].images.standard_resolution.url + "' " + "alt='" + json.data[i].caption.text + "'><figcaption><p><img src='instagram-logo.png'><br />" + json.data[i].caption.text + "<br /><span><img src='heart.png'>" + json.data[i].likes.count + "</span></p></figcaption></a></figure>");
+        }
+    })
+
+
+});
